f9771f2f9453dbadc9fb9e7105b547c9d3c31350
Katherine Thongsakounh <kthongsakounh@gmail.com>
2017-06-22 22:49:17 -0500

    Add vampire questionaire.

---
+++ ruby/vampires.rb
@@ -0,0 +1,18 @@
+# ask for vampire's name
+puts "What is your name?"
+name = gets.chomp
+
+# ask for vampire's age
+puts "How old are you? What year were you born?"
+age = gets.chomp
+year = gets.chomp
+
+# ask if employee wants garlic bread
+puts "Our company cafeteria serves garlic bread."
+puts "Should we order some for you?"
+garlic_bread = gets.chomp
+
+# ask if employee would like health insurance
+puts "Would you like to enroll in the company’s health insurance?"
+health_ins = gets.chomp
+
