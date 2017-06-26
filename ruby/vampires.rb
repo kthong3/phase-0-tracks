d2fae9c0be9277dfb7ddc1a7bd9ffda287841b40
Katherine Thongsakounh <kthongsakounh@gmail.com>
2017-06-26 00:43:38 -0500

    Add loop to ask employee to list any allergies.

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -22,7 +22,7 @@ until count == numberofemployees
     # ask if employee wants garlic bread
     puts "Our company cafeteria serves garlic bread."
     puts "Should we order some for you?"
-    garlic_bread = gets.chomp.downcase
+    garlic_bread = gets.chomp
       if garlic_bread.downcase == "yes" || garlic_bread.downcase == "no"
         valid_garlic_bread = true
       else
@@ -34,7 +34,7 @@ until count == numberofemployees
     until valid_input_health
     # ask if employee would like health insurance
     puts "Would you like to enroll in the company’s health insurance?"
-    health_ins = gets.chomp.downcase
+    health_ins = gets.chomp
       if health_ins.downcase == "yes" || health_ins.downcase == "no"
         valid_input_health = true
       else
@@ -42,24 +42,49 @@ until count == numberofemployees
       end
   end
   
+    valid_allergy = false
+    until valid_allergy
+    puts "Do you have any allergies?"
+      allergy = gets.chomp
+      if allergy.downcase == "yes" || allergy.downcase == "no"
+        valid_allergy= true
+      else
+        puts "I did not understand. Please type yes or no."
+      end
+    end
+    
+    if allergy.downcase == "yes"
+      puts "Please enter an allergy."
+        allergy_input = gets.chomp
+      
+        allergy_next = ""
+        until allergy_next.downcase == "done"
+          puts "Please enter another allergy or type Done."
+          allergy_next = gets.chomp
+        end
+    end
+    
   # find out if employee is vampire
   employeeisvampire = true
   while employeeisvampire
-    if (age.to_i <= 120 && year.to_i >= 1900 && garlic_bread.downcase == "yes" && health_ins.downcase == "yes") && (name !=	"Drake Cula" && name != "Tu Fang")
-    	puts "Probably not a vampire."
-    	employeeisvampire = false
-    elsif ((age.to_i >= 150 || year.to_i <= 1900) && garlic_bread.downcase == "no") || health_ins.downcase == "no"
-    	puts "Probably a vampire."
-    	employeeisvampire = false
+    if (age.to_i <= 120 && year.to_i >= 1900 && garlic_bread.downcase == "yes" && health_ins.downcase == "yes" && allergy_input.downcase != "sunshine" && allergy_next.downcase != "sunshine") && (name !=  "Drake Cula" && name != "Tu Fang")
+      puts "Probably not a vampire."
+      employeeisvampire = false
+    elsif (age.to_i >= 150 && year.to_i <= 1900 && garlic_bread.downcase == "no") || health_ins.downcase == "no"
+      puts "Probably a vampire."
+      employeeisvampire = false
     elsif age.to_i >= 150 && year.to_i <= 1900 && garlic_bread.downcase == "no" && health_ins.downcase == "no"
-    	puts "Almost certainly a vampire."
-    	employeeisvampire = false
-    elsif name ==	"Drake Cula" || name == "Tu Fang"
-    	puts "Definitely a vampire."
-    	employeeisvampire = false
+      puts "Almost certainly a vampire."
+      employeeisvampire = false
+    elsif name == "Drake Cula" || name == "Tu Fang"
+      puts "Definitely a vampire."
+      employeeisvampire = false
+    elsif allergy_input.downcase == "sunshine" || allergy_next.downcase = "sunshine"
+      puts "Probably a vampire."
+      employeeisvampire = false
     else
-    	puts "Results inconclusive."
-    	employeeisvampire = false
+      puts "Results inconclusive."
+      employeeisvampire = false
     end
   end
   
@@ -78,6 +103,8 @@ until count == numberofemployees
   else
     puts "The employee did not sign up for health insurance."
   end
+  
+  puts "The employee has the following allergies: #{allergy_input}"
   
   count += 1
   
