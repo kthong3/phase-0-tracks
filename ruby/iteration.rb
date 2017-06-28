3d6faffd00186ddc30d9d4d90183542be4d9f12b
Katherine Thongsakounh <kthongsakounh@gmail.com>
2017-06-28 17:06:09 -0500

    Created .each and .map for array.

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -5,3 +5,28 @@ def print_animal
 
 print_animal { |animal1,animal2| puts "Here is #{animal1}, and #{animal2}" }
 
+animals = [
+  "lion",
+  "deer",
+  "fox"]
+
+animals.each do |animal_type|
+ puts animal_type
+end
+
+puts "original data"
+p animals
+
+animals.map! do |animal|
+  animal.capitalize
+end
+
+puts "after .map"
+p animals
+puts animals
+
+movies_with_animals = {
+  lion: "Lion King",
+  deer: "Bambi",
+  fox: "Fox and the Hound"
+}
