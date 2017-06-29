6066492d900f1018daaf870d7d1c018dc41b28a3
Katherine Thongsakounh <kthongsakounh@gmail.com>
2017-06-28 20:27:20 -0500

    Ask for client's name and add name to hash.

---
+++ ruby/hashes.rb
@@ -0,0 +1,20 @@
+# client information
+# enter client's personal information: name, age, # of children,
+# decor theme, color theme preference (max of 3 colors), 
+# number of rooms to design, is client open to suggestion?, 
+# and is there a time limit?
+
+# hash program
+
+# ask for client information
+# this information will be added to client_information hash
+client_information = {}
+
+# ask for client's name
+puts "What is your name?"
+name = gets.chomp
+client_information[name] = name
+
+# ask for client's age
+client_information
+
