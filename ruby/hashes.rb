70314b7335b4e1447306aaee9e2e6da1daa8bb22
Katherine Thongsakounh <kthongsakounh@gmail.com>
2017-06-28 20:35:33 -0500

    Ask for number of children and decor theme to add to hash.

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -20,5 +20,19 @@ puts "What is your age?"
 age = gets.chomp
 client_information[:age] = age
 
+# ask for client's number of children
+puts "How many children do you have?"
+num_of_children = gets.chomp.to_i
+client_information[:num_of_children] = num_of_children
+
+# ask client for what decor theme they want
+puts "What type of decor theme would you like?"
+decor_theme = gets.chomp
+client_information[:decor_theme] = decor_theme
+
+
+
+
+
 
 
