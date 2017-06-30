5d1cb76526e8232dc0c277765747006a596895d0
kathiee <kthongsakounh@gmail.com>
2017-06-30 15:56:09 -0500

    Update problem_solving_method.rb

--- ruby/problem_solving_method.rb
+++ ruby/problem_solving_method.rb
@@ -12,15 +12,14 @@ array = [14, 24, 34, 44, 54]
 def search(array, n)
 	integer = nil
 	array.length.times do |i|
-		if array[i] == n
-			integer = i
-		end
+			if array[i] == n
+				integer = i
+			end
 	end 
 	p integer
 end
 
 search(array, 44)
-
 
 # initiate array that is empty but intialize with 100
 # creater method that starts w 0 and 1 set to variables and add
@@ -82,7 +81,7 @@ array = [1,4,7,9,4,3,5,7]
 
 def bubble_sort(array)
 	number = array.length
-
+	
 	loop do
 		sorted = false 
     # while this array is NOT sorted
@@ -92,20 +91,16 @@ def bubble_sort(array)
         # first index. if the 2nd element is greater than the 
         # next element, they will swap indexes
         if array[i] > array[i+1]
-
-          # to switch the two elements in the array index
+					# to switch the two elements in the array index
           array[i], array[i+1] = array[i+1], array[i]
-          
           # loop will continue to swap the elements until reaches the
           # end of the array or its proper place
           sorted = true
         end
     end
-
-    break if not sorted
+		break if not sorted
     # break after the array has been sorted
-  end
-
+	end
   # output the sorted array  
   array
 end
