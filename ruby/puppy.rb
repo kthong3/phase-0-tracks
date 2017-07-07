7af38a596137203eca3c20af69637c1aae2662e5
Katherine Thongsakounh <kthongsakounh@gmail.com>
2017-07-07 18:24:43 -0500

    Add puppy class characteristics and behaviors.

---
+++ ruby/puppy.rb
@@ -0,0 +1,15 @@
+# SPECIES ------------------------
+# Khao
+
+# CHARACTERISTICS ----------------
+# Friendly: yes
+# Eye count: 2
+# Name: varies
+# Color: white, brown
+# Weight: 5-10 lbs
+ 
+# BEHAVIOR -----------------------
+# Jump
+# Lick
+# Chase
+
