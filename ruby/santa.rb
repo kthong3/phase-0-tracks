30af5dc117481d8e0bb5e6d082df44f8512a0c75
Katherine Thongsakounh <kthongsakounh@gmail.com>
2017-07-10 16:38:14 -0500

    Add setter method to change gender and getter methods to return age and ethnicity.

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -15,10 +15,6 @@ class Santa
     @age = 0
   end
 
-  def age
-    @age
-  end
-
   def celebrate_birthday=(new_age)
     @age = new_age
   end
@@ -28,6 +24,20 @@ class Santa
     @reindeer_ranking.insert(-1, @reindeer_ranking.delete_at(@reindeer_ranking.index(reindeer_name)))
     # takes argument and deletes the index in array and inserts argument at the end of array
     p @reindeer_ranking
+  end
+
+  def gender_change=(new_gender)
+    @gender = new_gender
+    # p @gender
+  end
+
+  # getter methods
+  def age
+    @age
+  end
+  
+  def ethnicity
+    @ethnicity
   end
 
 end
@@ -41,15 +51,37 @@ genders.length.times do |i|
   santas << Santa.new(genders[i], ethnicities[i])
 end
 
-santas.each do |x|
-  puts "Santa is currently age #{x.age}"
-end
-
+# check to see if setter methods worked
 santas.each do |x|
   x.celebrate_birthday = 1
   puts "Let's celebrate Santa's birthday! Santa is now age #{x.age}"
 end
+# ==> Let's celebrate Santa's birthday! Santa is now age 1.
 
 santas.each do |x|
   x.get_mad_at("Vixen")
 end
+# ==> @reindeer_ranking=["Rudolph", "Dasher", "Dancer", "Prancer", "Comet", "Cupid", "Donner", "Blitzen", "Vixen"]
+
+santas.each do |x|
+  x.gender_change= "agender"
+end
+# ==> @gender="agender"
+
+# check to see if getter methods worked
+santas.each do |x|
+  puts "Santa is currently age #{x.age}."
+end
+# ==> Santa is currently age 1.
+# It changed to 1 since the setter method changed it
+
+santas.each do |x|
+  puts "Santa is #{x.ethnicity}."
+end
+# ==> Santa is Puerto Rican.
+# ==> Santa is French.
+# ==> Santa is Thai.
+# ==> Santa is Russian.
+# ==> Santa is Black.
+# ==> Santa is White.
+# ==> Santa is Lao.
