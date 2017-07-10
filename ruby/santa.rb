eef57575165d18b81be24adf4eeb52189fbbcde6
Katherine Thongsakounh <kthongsakounh@gmail.com>
2017-07-10 16:27:23 -0500

    Add setter method to move reindeer to end of reindeer rankings.

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -23,6 +23,13 @@ class Santa
     @age = new_age
   end
 
+  # setter method to take reindeer's name and move to end of array
+  def get_mad_at(reindeer_name)
+    @reindeer_ranking.insert(-1, @reindeer_ranking.delete_at(@reindeer_ranking.index(reindeer_name)))
+    # takes argument and deletes the index in array and inserts argument at the end of array
+    p @reindeer_ranking
+  end
+
 end
 
 santas = []
@@ -40,5 +47,9 @@ end
 
 santas.each do |x|
   x.celebrate_birthday = 1
-  puts "Let's celebreat Santa's birthday! Santa is now age #{x.age}"
+  puts "Let's celebrate Santa's birthday! Santa is now age #{x.age}"
+end
+
+santas.each do |x|
+  x.get_mad_at("Vixen")
 end
