7b029dab6fee2ec29caa9f476861042d997ab4ce
Katherine Thongsakounh <kthongsakounh@gmail.com>
2017-07-11 12:36:52 -0500

    Initalize new Santa instances to randomly choose from gender and ethnicity arrays.

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,7 +1,5 @@
 class Santa
   attr_reader :ethnicity
-  # shortcut for getter methods
-
   attr_accessor :age, :gender
 
   def speak
@@ -17,12 +15,9 @@ class Santa
     @gender = gender
     @ethnicity = ethnicity
     @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-    @age = 0
+    @age = rand(0..140)
+      # chooses random age between 0-140
   end
-
-  # def celebrate_birthday=(new_age)
-  #  @age = new_age
-  # end
 
   # setter method to take reindeer's name and move to end of array
   def get_mad_at(reindeer_name)
@@ -30,70 +25,19 @@ class Santa
     # takes argument and deletes the index in array and inserts argument at the end of array
     p @reindeer_ranking
   end
-
-  # def gender_change=(new_gender)
-  #  @gender = new_gender
-  #  p @gender
-  # end
-
-  # getter methods
-  # def age
-  #  @age
-  # end
-  
-  # def ethnicity
-  #  @ethnicity
-  # end
 end
 
-santas = []
-genders = ["male", "female", "transgender", "agender", "female", "bigender", "N/A"]
-ethnicities = ["Puerto Rican", "French", "Thai", "Russian", "Black", "White", "Lao"]
+santa = "Santa" + rand(1..1000).to_s
+gender = ["male", "female", "transgender", "agender", "bigender", "N/A", "gender fluid"]
+ethnicity = ["Puerto Rican", "French", "Thai", "Russian", "Black", "White", "Lao", "Mexican", "N/A", "Indian", "Australian", "German", "Polish"]
 
-genders.length.times do |i|
-  puts "Adding new Santa.."
-  santas << Santa.new(genders[i], ethnicities[i])
-end
+santa = Santa.new(gender.sample, ethnicity.sample)
+puts santa
+puts "Santa is #{santa.gender}."
+puts "Santa's ethnicity is #{santa.ethnicity}."
 
-santas.each do |x|
-  x.speak
-  x.eat_milk_and_cookies("snickerdoodle")
-end
+santa = Santa.new(gender.sample, ethnicity.sample)
+puts santa
+puts "Santa is #{santa.gender}."
+puts "Santa's ethnicity is #{santa.ethnicity}."
 
-# check to see if setter methods worked
-santas.each do |x|
-  x.age = 1
-  # x.celebrate_birthday = 1
-  puts "Let's celebrate Santa's birthday! Santa is now age #{x.age}"
-end
-# ==> Let's celebrate Santa's birthday! Santa is now age 1.
-
-santas.each do |x|
-  x.get_mad_at("Vixen")
-end
-# ==> @reindeer_ranking=["Rudolph", "Dasher", "Dancer", "Prancer", "Comet", "Cupid", "Donner", "Blitzen", "Vixen"]
-
-santas.each do |x|
-  x.gender = "agender"
-  puts "Santa is #{x.gender}."
-  # x.gender_change= "agender"
-end
-# ==> "Santa is agender."
-
-# check to see if getter methods worked
-santas.each do |x|
-  puts "Santa is currently age #{x.age}."
-end
-# ==> Santa is currently age 1.
-# It changed to 1 since the setter method changed it
-
-santas.each do |x|
-  puts "Santa is #{x.ethnicity}."
-end
-# ==> Santa is Puerto Rican.
-# ==> Santa is French.
-# ==> Santa is Thai.
-# ==> Santa is Russian.
-# ==> Santa is Black.
-# ==> Santa is White.
-# ==> Santa is Lao.
