fa2788c9e04355bccb49793e27b9eed09d6b514c
Katherine Thongsakounh <kthongsakounh@gmail.com>
2017-07-11 22:12:36 -0500

    Add yell_happily method and add driver code to call both methods in module.

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,15 @@
 module Shout
-  # we'll put some methods here soon, but this code is fine for now!
+  def self.yell_angrily(words)
+    words + "!!!" + " :("
+  end
+
+  def self.yell_happily(words)
+    words + " YAY! :)"
+  end
 end
+
+puts Shout.yell_angrily("I'm hungry")
+# ==> "I'm hungry!!! :("
+
+puts Shout.yell_happily("I'm so happy")
+# ==> "I'm so happy YAY! :)"
