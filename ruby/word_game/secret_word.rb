0c7ee9ff778d55dd1f325c68d096d3f2fc168f77
Katherine Thongsakounh <kthongsakounh@gmail.com>
2017-07-13 21:20:57 -0500

    Initialize new game and return underscore for each letter of secret word.

--- ruby/word_game/secret_word.rb
+++ ruby/word_game/secret_word.rb
@@ -7,20 +7,42 @@
 # If 2nd user is able to guess the word correctly before the whole secret word has appeared,
 #   a message will appear to congratulate the 2nd user.
 # Otherwise, if the user is not able to guess the word after all letters of the secret word 
-#   have appeared, a taunting message will appear for 2nd user.
+  # have appeared, a taunting message will appear for 2nd user.
 
 # BUSINESS LOGIC
 # Create Game class
 # Game.new will initialize an empty string
-# Method: a random word will be chosen by 1st user and change empty string to the secret word
-#   method will print out _ for each letter that is in the secret word
-# Method: take a string from the secret words array and split it into individual characters to iterate through
-# Method: while user is guessing the word, after each guess, add a character from secret word to print until all the letters have printed
+# Method: a random word will be chosen by 1st user and change empty string to the secret
+#   word. Method will print out a blank space _ for each letter that is in the secret word
+# Method: take a string from the secret words array and split it into individual characters
+#   to iterate through
+# Method: while user is guessing the word, after each guess, add a character from secret
+#   word to print until all the letters have printed
+class Game
+  def initialize(secret_word)
+    @secret_word = secret_word
+  end
+
+  def blank_letters
+    letters = @secret_word.length
+    p "_ " * letters
+  end
+end
+
+
 
 # USER INTERFACE
-# Ask 1st user to enter a secret word. 2nd user must turn around and/or look away while 1st user is typing secret word.
+# Ask 1st user to enter a secret word. 2nd user must turn around and/or look away while the
+#   1st user is typing secret word.
 # Ask 2nd user to guess the secret word
-# loop: While the 2nd user has not input in the correct word,
-  # program will continually output one letter of secret word until each letter of secret word has appeared
+# loop: While the 2nd user has not input in the correct word, program will continually
+#   output one letter of secret word until each letter of secret word has appeared
 # If user guesses the word correctly before the secret word appears, user wins
 # If user doesn't ever guess correctly before the secret word appears, user loses
+puts "Let's play a guessing game!"
+puts "Player 2, please turn around while Player 1 enters secret word."
+puts "Player 1, please enter your secret word:"
+# game = Game.new(secret_word)
+
+puts "Player 2, please guess the word:"
+# game.blank_letters
--- ruby/word_game/secret_word_spec.rb
+++ ruby/word_game/secret_word_spec.rb
@@ -1,1 +1,9 @@
 require_relative 'secret_word'
+
+describe Game do
+let (:game) {Game.new("cat")}
+  
+  it "returns same number of blank spaces for length of secret word" do
+    expect(game.blank_letters).to eq "_ _ _ "
+  end
+end
