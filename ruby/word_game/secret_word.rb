62f6c853041fcf4c0493656a2cf019536b0fc1fb
Katherine Thongsakounh <kthongsakounh@gmail.com>
2017-07-15 18:54:39 -0500

    Update pseudocode to show that user will only guess words, not letters.

--- ruby/word_game/secret_word.rb
+++ ruby/word_game/secret_word.rb
@@ -1,19 +1,32 @@
-# Create a game class for word guessing game
-# Game will allow 2 users to play a word guessing game
-# 1st user will enter a secret word.
-# Each letter of the secret word will appear as a blank space _.
-# The number of blank spaces will determine how many guesses 2nd user will be allowed.
-# Repeated guesses will not count against the 2nd user.
-# After each letter guess, a new letter of 1st user's secret word will appear. 
-# If 2nd user is able to guess the word correctly before all of the letters have appeared,
-#   a message will appear to congratulate the 2nd user.
-# Otherwise, if the user is not able to guess the word after all letters of the secret word 
-#   have appeared, a taunting message will appear for 2nd user.
+# Word guessing game
+# Allow 2 users to play a word guessing game
+# Allow 1st user will enter a secret word. 
+# The number of letters in the secret word will determine the number of guesses for the 2nd user.
+# Allow 2nd user to guess the word. Repeated guesses will not count against the 2nd user.
+# After each incorrect word guess, a new letter of the secret word will appear.
+# If 2nd user guesses the word correctly before the whole secret word appears, 2nd user wins
+# If 2nd user does not guess correctly before the secret word appears, 2nd user loses
 
 # BUSINESS LOGIC
-# Method: iterate through string and randomly choose a letter to insert
-# Method: while user is guessing the word, after each guess, add a character from secret
-#   word to print until all the letters have printed
+# Method to turn secret word into blank spaces
+# input: secret word
+# steps: determine number of letters of secret word
+# output: A blank space _ will print for each letter that is in the secret word
+
+# Method to store guessed words and find if any have been duplicated
+# input: guessed word
+# steps: Iterate through list of guessed words to see if word is already contained in list of guessed words, if it is not, add guessed word to list
+# output: Return true if duplicate found or false if there is no duplicate
+
+# Method to show guessed words after each guess
+# input: guessed word
+# steps: Put guessed word in a list to show all guessed words
+# output: Return each guessed word after each guess.
+
+# Method to reveal one letter of secret word if secret word is not guessed correctly
+# input: secret word, guessed word
+# steps: Check to see guess word and secret word are the same, if not, after each wrong guess, reveal one letter of the secret word
+# output: A letter of the secret word and blank spaces
 class Game
   def initialize(secret_word)
     @secret_word = secret_word
@@ -26,20 +39,10 @@ class Game
     p "_ " * letters
   end
 
-
 end
 
 
 # USER INTERFACE
-# Ask 1st user to enter a secret word. 2nd user must turn around and/or look away while the
-#   1st user is typing secret word.
-# Ask 2nd user to guess the secret word
-
-# loop: length of secret word will determine number amount of guesses 2nds user has but 
-#   excluding any repeated letters
-# If user guesses the word correctly before the secret word appears, 2nd user wins
-# If user doesn't ever guess correctly before the secret word appears, 2nd user loses
-
 puts "Let's play a guessing game!"
 puts "Player 2, please turn around while Player 1 enters secret word."
 puts "Player 1, please enter your secret word:"
