cb8ce91aac36d8e612e0aabf498a4f9cfdcf3bf5
Katherine Thongsakounh <kthongsakounh@gmail.com>
2017-07-16 12:14:59 -0500

    Add class methods to pass rspec tests.

--- ruby/list/todolist.rb
+++ ruby/list/todolist.rb
@@ -1,2 +1,36 @@
+# PSUEDOCODE
+  # Initialize method initializes an array including parameter
+    # input: array
+    # output: array
+  # Create method to add an item to existing list
+    # input: string
+    # output: array
+  #  Create  method to delete an item from existing list
+    # input: string
+    # output: array
+  # Create method to retrieve an index from existing list
+    # input: integer
+    # output: string
+
+# CLASS DECLARATION
 class TodoList
+  attr_accessor :get_items
+
+  def initialize(list)
+    @get_items = list
+  end
+
+  def add_item(item)
+    @get_items << item
+  end
+
+  def delete_item(item)
+    @get_items.delete(item)
+    @get_items
+  end
+
+  def get_item(index)
+    @get_items[index]
+  end
 end
+
