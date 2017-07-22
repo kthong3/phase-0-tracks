e8b1d8b8532684a23e2fa530d151c3433f4fb6fa
Katherine Thongsakounh <kthongsakounh@gmail.com>
2017-07-22 12:51:36 -0500

    Add pseudocode for function to find longest phrase in array.

---
+++ js/algos.js
@@ -0,0 +1,6 @@
+// write a function that takes an array of words or phrases and returns the longest 
+//  word or phrase in the array
+// input: array of phrases (strings)
+// steps: iterate through the array to find the length of each index of the array
+// output: return the longest phrase (strings)
+
