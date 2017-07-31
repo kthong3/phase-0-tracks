d1d4c451431b08fb4d6038ed1306becc9326d399
Katherine Thongsakounh <kthongsakounh@gmail.com>
2017-07-30 23:31:00 -0500

    Add command to create tasks table.

--- databases/build_something/task_list.rb
+++ databases/build_something/task_list.rb
@@ -6,3 +6,15 @@ require 'sqlite3'
 db = SQLite3::Database.new("tasklist.db")
 db.results_as_hash = true
 
+# create a command in SQL
+create_table_cmd = <<-SQL
+  CREATE TABLE IF NOT EXISTS tasks (
+    id INTEGER PRIMARY KEY,
+    task VARCHAR(255),
+    priority INT,
+    status VARCHAR(255) 
+  )
+SQL
+
+# create the tasks table
+db.execute(create_table_cmd)
