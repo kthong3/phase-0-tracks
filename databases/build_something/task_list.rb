b2256bf40ba760f62b4e0e0eb8fec267234889f8
Katherine Thongsakounh <kthongsakounh@gmail.com>
2017-07-31 00:41:21 -0500

    Add priority and status table data for foreign keys in tasks table.

--- databases/build_something/task_list.rb
+++ databases/build_something/task_list.rb
@@ -6,15 +6,43 @@ require 'sqlite3'
 db = SQLite3::Database.new("tasklist.db")
 db.results_as_hash = true
 
-# create a command in SQL
-create_table_cmd = <<-SQL
+# create commands in SQL
+create_tasks_table = <<-SQL
   CREATE TABLE IF NOT EXISTS tasks (
     id INTEGER PRIMARY KEY,
     task VARCHAR(255),
-    priority INT,
-    status VARCHAR(255) 
+    priority_id INT,
+    status_id INT,
+  	FOREIGN KEY (priority_id) REFERENCES priority(id),
+  	FOREIGN KEY (status_id) REFERENCES status(id)
   )
 SQL
 
-# create the tasks table
-db.execute(create_table_cmd)
+create_priority_table = <<-SQL
+  CREATE TABLE IF NOT EXISTS priority (
+    id INTEGER PRIMARY KEY,
+    priority VARCHAR(255)
+  )
+SQL
+
+create_status_table = <<-SQL
+  CREATE TABLE IF NOT EXISTS status (
+    id INTEGER PRIMARY KEY,
+    status VARCHAR(255)
+  )
+SQL
+
+# create the tasks, priority and status tables
+db.execute(create_tasks_table)
+db.execute(create_priority_table)
+db.execute(create_status_table)
+
+# add priorities to priority list
+db.execute("INSERT INTO priority (priority) VALUES ('high')")
+db.execute("INSERT INTO priority (priority) VALUES ('medium')")
+db.execute("INSERT INTO priority (priority) VALUES ('low')")
+
+# add statuses to status list
+db.execute("INSERT INTO status (status) VALUES ('to-do')")
+db.execute("INSERT INTO status (status) VALUES ('in-progress')")
+db.execute("INSERT INTO status (status) VALUES ('completed')")
